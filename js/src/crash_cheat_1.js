68e826ac4306ac4b1bbbd0963ab26c7d0b14cb15
MaxHuang26 <maxhuangming@hotmail.com>
2019-07-17 17:35:20 +0800

    Site updated: 2019-07-17 17:35:19

--- js/src/crash_cheat_1.js
+++ js/src/crash_cheat_1.js
@@ -11,35 +11,24 @@
       
  document.addEventListener('visibilitychange', function () {
 	 var b = myRandom();
+	 var x = null;
+	 if(x==null){
+	 x=b;}
 	 if(b==1){
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
          document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
-     else {
-         $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~';
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-
-     }
 	 }
-	 else if (b==2){
+
+	  else if (b==2){
 		      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
          document.title = '(งᵒ̌皿ᵒ̌)ง⁼³₌₃ 别跑呀！！ ~';
          clearTimeout(titleTime);
      }
-     else {
-         $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
-         document.title = '((٩(´͈ᗨ`͈)۶)) 回来啦~抱一个';
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-
-	 }
+   
 	 }
 	 else if (b==3){
 		      if (document.hidden) {
@@ -47,15 +36,48 @@
          document.title = '(/∇＼*) 我藏好啦 ~';
          clearTimeout(titleTime);
      }
-     else {
+     
+	 }
+	 
+	 
+	 
+	 	 if(x==1){
+     if (!document.hidden) {
          $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+		 x=null;
+     }
+	 }
+
+	  else if (x==2){
+		  if (!document.hidden) {
+		      $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
+         document.title = '((٩(´͈ᗨ`͈)۶)) 回来啦~抱一个';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+		 x=null;
+
+     }
+   
+	 }
+	 else if (x==3){
+		 if (!document.hidden) {
+ $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
          document.title =  '(*๓´╰╯`๓) 嗨呀~被找到啦';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
-
+		 x=null;
      }
+     
 	 }
+	 
+	 }
+	
  });
 
 	  
