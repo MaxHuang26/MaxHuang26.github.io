27735d4443d0fed32ba54cbfb61f704a9189421f
MaxHuang26 <maxhuangming@hotmail.com>
2019-07-17 09:14:04 +0800

    Site updated: 2019-07-17 09:13:56

---
+++ js/src/crash_cheat_2.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '(งᵒ̌皿ᵒ̌)ง⁼³₌₃ 别跑呀！！ ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '((٩(´͈ᗨ`͈)۶)) 回来啦~抱一个' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
---
+++ js/src/crash_cheat_3.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '(/∇＼*) 我藏好啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(*๓´╰╯`๓) 嗨呀~被找到啦' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
