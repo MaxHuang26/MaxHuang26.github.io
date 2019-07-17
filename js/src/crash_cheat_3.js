09bce41600dcea433a921ae8ebb86d79ab5253e4
MaxHuang26 <maxhuangming@hotmail.com>
2019-07-17 10:38:52 +0800

    Site updated: 2019-07-17 10:38:51

--- js/src/crash_cheat_3.js
+++ js/src/crash_cheat_3.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.ico");
          document.title = '(/∇＼*) 我藏好啦 ~';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.ico");
          document.title = '(*๓´╰╯`๓) 嗨呀~被找到啦' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
