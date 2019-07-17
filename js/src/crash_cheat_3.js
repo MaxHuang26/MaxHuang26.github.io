0e57d404ccfb83cd710b5feba673283c0318e0bf
MaxHuang26 <maxhuangming@hotmail.com>
2019-07-17 12:40:27 +0800

    Site updated: 2019-07-17 12:40:24

--- js/src/crash_cheat_3.js
+++ js/src/crash_cheat_3.js
@@ -1,7 +1,17 @@
 <!--崩溃欺骗-->
  var OriginTitle = document.title;
  var titleTime;
+ function myRandom() {
+ var rand = Math.random();
+ if (rand < .5) return 1;
+ if (rand < .8) return 2;
+ if (rand < .95) return 3;
+}
  document.addEventListener('visibilitychange', function () {
+	 while(true){
+		 int i = myRandom();
+	 switch{
+		 case(i==1){
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
          document.title = '(/∇＼*) 我藏好啦 ~';
@@ -9,9 +19,40 @@
      }
      else {
          $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
-         document.title = '(*๓´╰╯`๓) 嗨呀~被找到啦' + OriginTitle;
+         document.title =  '(*๓´╰╯`๓) 嗨呀~被找到啦';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
      }
+		 }break;
+		 		 case(i==2){
+          if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/MaxHuang26.github.io/images/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+		 }break;
+		 		 case(i==3){
+          if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '(งᵒ̌皿ᵒ̌)ง⁼³₌₃ 别跑呀！！ ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '((٩(´͈ᗨ`͈)۶)) 回来啦~抱一个';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+		 }break;
+	 }
+	 }
  });
