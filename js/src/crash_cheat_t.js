9b9d9de7a7357e62a3d5f105bdcb00bc45cafefd
MaxHuang26 <maxhuangming@hotmail.com>
2019-07-18 09:47:38 +0800

    Site updated: 2019-07-18 09:47:33

---
+++ js/src/crash_cheat_t.js
@@ -0,0 +1,31 @@
+        var OriginTitile = document.title;
+		var titleTime = null;
+		var titleIndex = 0;
+		title = [{
+			blur: "(●—●) 哎呦，崩溃啦！",
+			focus: "o(≧∇≦o) 啊咧，又好了……"
+		}, {
+			blur: "(つェ⊂) 看不到我～",
+			focus: "(*´∇｀*) 被发现啦～"
+		}, {
+			blur: "(>_<) 我藏好了哦",
+			focus: "(*´∇｀*) 被发现啦～"
+		}];
+
+	$(document).on("visibilitychange", function() {
+		if (document.hidden) {
+			titleIndex = Math.floor(Math.random() * 3);
+			document.title = title[titleIndex].blur;
+			if (titleTime) {
+				clearTimeout(titleTime);
+				titleTime = null;
+			}
+		}
+		else {
+			document.title = title[titleIndex].focus;
+			showMessage("哇，你又回来了～", 6000, 9);
+			titleTime = setTimeout(function() {
+				document.title = OriginTitile;
+			}, 3000);
+		}
+	});
