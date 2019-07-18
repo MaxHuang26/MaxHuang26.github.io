3cfd4f6ce837cf5095c933f99d53ea89ed0222f4
MaxHuang26 <maxhuangming@hotmail.com>
2019-07-18 10:24:52 +0800

    Site updated: 2019-07-18 10:24:51

--- js/src/crash_cheat_w.js
+++ js/src/crash_cheat_w.js
@@ -10,11 +10,16 @@
 		}, {
 			blur: "(>_<) 我藏好了哦",
 			focus: "(*´∇｀*) 被发现啦～"
-		}];
+		},
+		{
+			blur:  "(งᵒ̌皿ᵒ̌)ง⁼³₌₃ 别跑呀！！ ~",
+			focus: "((٩(´͈ᗨ`͈)۶)) 回来啦~抱一个"
+		}
+		];
 
 	document.onvisibilitychange= function() {
 		if (document.hidden) {
-			titleIndex = Math.floor(Math.random() * 3);
+			titleIndex = Math.floor(Math.random() * 4);
 			document.title = title[titleIndex].blur;
 			if (titleTime) {
 				clearTimeout(titleTime);
